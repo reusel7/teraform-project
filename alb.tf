5db3cd3273b07736ae82a28d98d1a4a9da2cb408
Artash <reusel7@yahoo.com>
2024-03-28 15:15:54 +0400

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -57,7 +57,7 @@ EOF
 resource "aws_instance" "artash_instance2" {
   ami           = "ami-0b8b44ec9a8f90422"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.artash_subnet_2.id
+  subnet_id     = aws_subnet.artash_subnet_1.id
   associate_public_ip_address = true
   security_groups    = [aws_security_group.artash_SG.id]
   user_data     = <<-EOF
