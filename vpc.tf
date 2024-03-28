619be3c41005fc5ded95ef0033adff2284f7a5ac
Artash <reusel7@yahoo.com>
2024-03-28 14:01:53 +0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,31 +9,31 @@ resource "aws_vpc" "artash_vpc" {
 }
 # Createing VPC first subnets ///////////////////////////////////////
 
-resource "aws_subnet" "artash_subnet_a" {
+resource "aws_subnet" "artash_subnet_1" {
   vpc_id            = aws_vpc.artash_vpc.id
   cidr_block        = "10.10.10.0/27"
   availability_zone = "us-east-2a"
 }
 
-resource "aws_subnet" "artash_subnet_b" {
+resource "aws_subnet" "artash_subnet_2" {
   vpc_id            = aws_vpc.artash_vpc.id
   cidr_block        = "10.10.10.32/27"
   availability_zone = "us-east-2a"
 }
 
-resource "aws_subnet" "artash_subnet" {
+resource "aws_subnet" "artash_subnet_3" {
   vpc_id            = aws_vpc.artash_vpc.id
   cidr_block        = "10.10.10.64/27"
   availability_zone = "us-east-2b"
 }
 
-resource "aws_subnet" "artash_subnet" {
+resource "aws_subnet" "artash_subnet_4" {
   vpc_id            = aws_vpc.artash_vpc.id
   cidr_block        = "10.10.10.96/27"
   availability_zone = "us-east-2b"
 }
 
-# Createing security_group allowed allowed all incommeing traffic to VPC ports ///////////////////////////
+# Create security_group allowed allowed all incoming traffic to VPC ports ///////////////////////////
 
 resource "aws_security_group" "artash_SG" {
   name        = "artash_SG"
